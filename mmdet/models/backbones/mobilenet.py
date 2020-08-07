c586175eb4d8aa7d1b716cbd9be79c0e9f3917ca
oulin <njuoulin@163.com>
2020-08-07 15:46:35 +0800

    add mobilenet backbone

---
+++ mmdet/models/backbones/mobilenet.py
@@ -0,0 +1,174 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models._utils as _utils
+import torchvision.models as models
+import math
+
+def _make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU(inplace=True)
+        )
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(round(inp * expand_ratio))
+        self.use_res_connect = self.stride == 1 and inp == oup
+
+        layers = []
+        if expand_ratio != 1:
+            # pw
+            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
+        layers.extend([
+            # dw
+            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim),
+            # pw-linear
+            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(oup),
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+@BACKBONES.register_module
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=1000, width_mult=1.0, inverted_residual_setting=None, round_nearest=8, out_indices=None,
+        frozen_stages=-1,norm_eval=True):
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        input_channel = 32
+        last_channel = 1280
+        self.out_indices = out_indices
+        self.frozen_stages = frozen_stages
+        self.norm_eval = norm_eval
+
+        if inverted_residual_setting is None:
+            inverted_residual_setting = [
+                # t, c, n, s
+                [1, 16, 1, 1],
+                [6, 24, 2, 2],
+                [6, 32, 3, 2],
+                [6, 64, 4, 2],
+                [6, 96, 3, 1],
+                [6, 160, 3, 2],
+                [6, 320, 1, 1],
+            ]
+
+        # only check the first element, assuming user knows t,c,n,s are required
+        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
+            raise ValueError("inverted_residual_setting should be non-empty "
+                             "or a 4-element list, got {}".format(inverted_residual_setting))
+
+        # building first layer
+        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
+        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
+
+        self.mobilenet_layer_name = []
+        self.stem = ConvBNReLU(3, input_channel, stride=2)
+
+        # features = [ConvBNReLU(3, input_channel, stride=2)]
+        # building inverted residual blocks
+        for i, (t, c, n, s) in enumerate(inverted_residual_setting):
+            layer = []
+            layer_name = 'layer{}'.format(i + 1)
+            self.mobilenet_layer_name.append(layer_name)
+            output_channel = _make_divisible(c * width_mult, round_nearest)
+            for j in range(n):
+                stride = s if j == 0 else 1
+                layer.append(block(input_channel, output_channel, stride, expand_ratio=t))
+                input_channel = output_channel
+            self.add_module(layer_name, nn.Sequential(*layer))
+        # building last several layers
+        if not self.out_indices:
+            # features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1))
+            self.last_layer = ConvBNReLU(input_channel, self.last_channel, kernel_size=1)
+        # make it nn.Sequential
+            # self.features = nn.Sequential(*features)
+
+        # building classifier
+            self.classifier = nn.Sequential(
+                nn.Dropout(0.2),
+                nn.Linear(self.last_channel, num_classes),
+        )
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+        
+    def init_weights(self, pretrained=None):
+        if isinstance(pretrained, str):
+            import torch
+            assert os.path.isfile(pretrained), "file {} not found.".format(pretrained)
+            self.load_state_dict(torch.load(pretrained), strict=False)
+        elif pretrained is None:
+            for m in self.modules():
+                if isinstance(m, nn.Conv2d):
+                    kaiming_init(m)
+                elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                    constant_init(m, 1)
+        else:
+            raise TypeError('pretrained must be a str or None')
+
+    def _freeze_stages(self):
+        if self.frozen_stages >= 0:
+            for param in self.stem.parameters():
+                param.requires_grad = False
+
+        for i in range(1, self.frozen_stages + 1):
+            m = getattr(self, 'layer{}'.format(i))
+            for param in m.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        x = self.stem(x)
+        outs = []
+        for i, layer_name in enumerate(self.mobilenet_layer_name):
+            mobilenet_layer = getattr(self, layer_name)
+            x = mobilenet_layer(x)
+            if self.out_indices and i in self.out_indices:
+                outs.append(x)
+        if not self.out_indices:
+            x = self.last_layer(x)
+            x = x.mean([2, 3])
+            outs = self.classifier(x)
+        return outs
+
+    def train(self, mode=True):
+        super(MobileNetV2, self).train(mode)
+        if mode and self.norm_eval:
+            for m in self.modules():
+                if isinstance(m, nn.BatchNorm2d):
+                    m.eval()
+
